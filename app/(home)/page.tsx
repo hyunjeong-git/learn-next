e4c88b3731747120b7c0b100575a603bd4dda3f3
HJ <wjdgusfb11@gmail.com>
2025-10-03 20:37:10 +0900

    feat: fetch movie data

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,31 @@
-// 홈페이지에만 표시
-export const metadata = {
-  title: "Home",
-};
+"use client";
+import { useEffect, useState } from "react";
 
+// 홈페이지에만 표시
+// client component에서 metadata 설정 불가
+// export const metadata = {
+//   title: "Home",
+// };
 
 export default function Pages() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://nomad-movies.nomadcoders.workers.dev/movies`
+    );
+    const json = await response.json();
+    setMovie(json);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div>
-      <h1>Home</h1>
+      {isLoading ? 'Loading...' : JSON.stringify(movie)}
     </div>
   );
 }
