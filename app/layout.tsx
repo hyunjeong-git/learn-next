64a580861e841a7a38e726f5fb97760a516ff79f
HJ <wjdgusfb11@gmail.com>
2025-10-07 01:09:25 +0900

    fix: import module 오류 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "../styles/global.css";
-import Navigation from "./components/navigation";
+import Navigation from "./components/Navigation";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
